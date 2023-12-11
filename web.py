b9d52b6e6ad56fc3c9817537a235aed8bb05557e
anusha <anushakizhakkeveedu@gmail.com>
2023-12-11 10:20:08 +0530

    Add web.py

--- web.py
+++ web.py
@@ -19,43 +19,38 @@ def employees():
     return flask.render_template("userlist.html", users=users)
 
 
-cache={}
 @app.route("/employees/<int:empid>", methods=["GET", "POST"])
 def employee_details(empid):
-    if empid in cache:
-        print(f"returning {empid} from cache")
-        return flask.jsonify(cache[empid])
-    else:
+    
         
-        query1 = db.select(models.Employee).where(models.Employee.id == empid)
-        user = db.session.execute(query1).scalar()
-        
-        query_for_leaves = db.select(func.count(models.Employee.id)).join(models.Leave, models.Employee.id == models.Leave.employee_id).filter(models.Employee.id == empid)
-        leave = db.session.execute(query_for_leaves).scalar()
+    query1 = db.select(models.Employee).where(models.Employee.id == empid)
+    user = db.session.execute(query1).scalar()
+    
+    query_for_leaves = db.select(func.count(models.Employee.id)).join(models.Leave, models.Employee.id == models.Leave.employee_id).filter(models.Employee.id == empid)
+    leave = db.session.execute(query_for_leaves).scalar()
 
-        query2 = db.select(models.Designation.max_leaves).where(models.Designation.id == models.Employee.title_id)
-        max_leaves = db.session.execute(query2).scalar()
+    query2 = db.select(models.Designation.max_leaves).where(models.Designation.id == models.Employee.title_id)
+    max_leaves = db.session.execute(query2).scalar()
 
-        if request.method == 'POST':
-            date = request.form.get['leavedate']
-            reason = request.form.get['leavereason']
-            new_leave = models.Leave(date=date, employee_id=empid, reason=reason)
-            db.session.add(new_leave)
-            db.session.commit()
-        ret = {
-            "id": user.id,
-            "fname": user.fname,
-            "lname": user.lname,
-            "title": user.title.title,
-            "email": user.email,
-            "phone": user.phone,
-            "leave": leave,
-            "max_leaves": max_leaves,
-            "remaining_leaves": max_leaves - leave
+    if request.method == 'POST':
+        date = request.form.get['leavedate']
+        reason = request.form.get['leavereason']
+        new_leave = models.Leave(date=date, employee_id=empid, reason=reason)
+        db.session.add(new_leave)
+        db.session.commit()
+    ret = {
+        "id": user.id,
+        "fname": user.fname,
+        "lname": user.lname,
+        "title": user.title.title,
+        "email": user.email,
+        "phone": user.phone,
+        "leave": leave,
+        "max_leaves": max_leaves,
+        "remaining_leaves": max_leaves - leave
 
-            }
-        cache['empid']= ret
-        return flask.jsonify(ret) 
+        }
+    return flask.jsonify(ret) 
 
 @app.route("/add_leave/<int:empid>", methods=["GET", "POST"])
 def add_leave(empid):
